d05a7ddeee66e4a78638ad25015744ff26e5c1e3
Melissa <anamelissa1707@gmail.com>
2024-09-01 13:03:33 -0700

    updating front

--- src/front.py
+++ src/front.py
@@ -2,64 +2,145 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-# Sample DataFrame
-df = pd.DataFrame({
-    'movie_title': ['Movie 12', 'Movie 23', 'Movie 31', 'Movie 11', 'Movie 123'],
-    'image_path': ['./433459342_433833065975660_5424982865884172125_n.jpg', './433459342_433833065975660_5424982865884172125_n.jpg','./433459342_433833065975660_5424982865884172125_n.jpg', './433459342_433833065975660_5424982865884172125_n.jpg', './433459342_433833065975660_5424982865884172125_n.jpg']
-})
+from recommender import Recommender
+from user import UserInteraction
 
-def load_films():
-    st.write("Loading films...")
+# Read the CSV file
+df = pd.read_csv('dataset/movies.csv')
+ratings_df = pd.read_csv('dataset/ratings.csv')
+user_ids = ratings_df['userId'].unique()
+
+
+movies = df[['movieId', 'title']]
+image_path = './movie_icon.jpg'
+user = None
+
+
+def load_films(movies_to_display):
     search_query = st.text_input("Search for a movie:")
     if search_query:
-        results = df[df['movie_title'].str.contains(search_query, case=False)]
-        num_results = len(results)
-        rows = (num_results // 3) + (num_results % 3 > 0)
-        
-        for row in range(rows):
-            cols = st.columns(3)
-            for col, idx in zip(cols, range(row * 3, min((row + 1) * 3, num_results))):
-                movie = results.iloc[idx]
-                col.write(movie['movie_title'])
-                img = Image.open(movie['image_path'])
-                col.image(img, caption=movie['movie_title'], use_column_width=True)
-                
-                if col.button(f"Rate {movie['movie_title']}", key=f"rate_button_{idx}"):
-                    with st.form(key=f"rating_form_{idx}"):
-                        rating = st.selectbox(f"Select your rating for {movie['movie_title']}", [1, 2, 3, 4, 5], key=f"rating_select_{idx}")
-                        submit_button = st.form_submit_button(label="Save Rating")
-                        if submit_button:
-                            df.at[idx, 'ratings'] = rating
-                            st.write(f"Rating for {movie['movie_title']}: {rating} stars")
+        results = movies_to_display[movies_to_display['title'].str.contains(search_query, case=False)].head(20)
+    else:
+        results = movies_to_display.head(20)
+
+    num_results = len(results)
+    rows = (num_results // 3) + (num_results % 3 > 0)
+    
+    for row in range(rows):
+        cols = st.columns(3)
+        for col, idx in zip(cols, range(row * 3, min((row + 1) * 3, num_results))):
+            movie = results.iloc[idx]
+            img = Image.open(image_path)
+            col.image(img, caption=movie['title'], use_column_width=True)
+            
+            if col.button(f"Rate {movie['title']}", key=f"rate_button_{idx}"):
+                with st.form(key=f"rating_form_{idx}"):
+                    rating = st.selectbox(f"Select your rating for {movie['title']}", [1, 2, 3, 4, 5], key=f"rating_select_{idx}")
+                    st.write(movie['movieId'])
+                    submit_button = st.form_submit_button(label="Save Rating")
+                    if submit_button:
+                        st.session_state.user.rate_movie(movie['movieId'], rating)
+                        st.write(f"Rating for {movie['title']}: {rating} stars")
 
 def create_user(user_name):
     # Placeholder for creating a new user
     st.write(f"Creating user: {user_name}")
     # Simulate user ID creation
-    return user_name + "_id"
+    user = UserInteraction()
+    return user
+
+def recommend_movies(user):
+    """
+    Crea un objeto Recommender para el usuario y utiliza este objeto para recomendar películas.
+    Muestra las recomendaciones en la aplicación Streamlit.
+    """
+    return user.get_recommendation()
 
 def main():
     st.title("Movie Recommendation System")
 
-    if 'user_id' not in st.session_state:
-        st.session_state.user_id = None
-
-    if st.session_state.user_id:
-        st.write(f"UserId: {st.session_state.user_id}")
-        load_films()
-    else:
-        user_name = st.text_input("Enter your user name:")
-        if user_name:
-            st.session_state.user_id = user_name + "_id"
-            st.write(f"UserId: {st.session_state.user_id}")
-            load_films()
+    if 'user' in st.session_state:
+        st.write(f"UserId: {st.session_state.user.user_id}")
+        
+        # Add the "Recommend Movies" button at the top right corner
+        recommend_button = st.button("Recommend Movies", key="recommend_button")
+        
+        if recommend_button:
+            recommended_movies = recommend_movies(st.session_state.user)
+            load_films(recommended_movies)
         else:
-            if st.button("Create New User"):
-                new_user_name = st.text_input("Enter your new user name:")
-                if new_user_name:
-                    st.session_state.user_id = create_user(new_user_name)
-                    st.write(f"UserId: {st.session_state.user_id}")
-                    load_films()
+            load_films(movies)
+
+    else:
+        if 'creating_new_user' in st.session_state:
+            new_user_name = st.text_input("Enter your new user name:")
+            if new_user_name:
+                user = create_user(new_user_name)
+                st.session_state.user = user
+                st.write(f"{new_user_name}, your id is: {st.session_state.user.user_id}")
+                st.session_state.creating_new_user = False
+                st.rerun()
+        else:
+            # user_id = st.text_input("Enter your user id:")
+            # Extract unique userIds and add a default option
+            user_ids = ratings_df['userId'].unique().tolist()
+            user_ids.insert(0, None)
+
+            # Create the select component with the default value
+            user_id = st.selectbox('Select User ID', user_ids)
+
+            if user_id:
+                st.session_state.user = UserInteraction(user_id)
+                st.write(f"UserId: {st.session_state.user.user_id}")
+                st.rerun()
+            else:
+                if st.button("Create New User"):
+                    st.session_state.creating_new_user = True
+                    st.rerun()
+
+                    # new_user_name = st.text_input("Enter your new user name:")
+                    # if new_user_name:
+                    #     user = create_user(new_user_name)
+                    #     st.session_state.user = user
+                    #     st.write(f"{new_user_name}, your id is: {st.session_state.user.user_id}")
+                    #     st.session_state.creating_new_user = False
+                    #     load_films(movies)
+    # if 'user_id' not in st.session_state:
+    #     st.session_state.user_id = None
+
+    # if 'creating_new_user' not in st.session_state:
+    #     st.session_state.creating_new_user = False
+
+    # if st.session_state.user_id:
+    #     st.write(f"UserId: {st.session_state.user_id}")
+        
+    #     # Add the "Recommend Movies" button at the top right corner
+    #     recommend_button = st.button("Recommend Movies", key="recommend_button")
+        
+    #     if recommend_button:
+    #         recommended_movies = recommend_movies(user)
+    #         load_films(recommended_movies)
+    #     else:
+    #         load_films(movies)
+    # else:
+    #     if st.session_state.creating_new_user:
+    #         new_user_name = st.text_input("Enter your new user name:")
+    #         if new_user_name:
+    #             user = create_user(new_user_name)
+    #             st.session_state.user_id = user.user_id
+    #             st.write(f"{new_user_name}, your id is: {st.session_state.user_id}")
+    #             st.session_state.creating_new_user = False
+    #             load_films(movies)
+    #     else:
+    #         user_id = st.text_input("Enter your user id:")
+    #         if user_id:
+    #             st.session_state.user_id = user_id
+    #             user = UserInteraction(user_id)
+    #             st.write(f"UserId: {st.session_state.user_id}")
+    #             load_films(movies)
+    #         else:
+    #             if st.button("Create New User"):
+    #                 st.session_state.creating_new_user = True
 
 if __name__ == "__main__":
     main()
@@ -94,3 +175,4 @@ if __name__ == "__main__":
 #     recommend_movies(user)
 
     
+
